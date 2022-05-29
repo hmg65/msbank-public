29e31d1f3e86ee4b13b3a372e41fc3a2a3a39c29
Hemant Gupta <gupta.hemant65@gmail.com>
2022-05-29 14:06:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -61,9 +61,6 @@ function App() {
                 }
               />
 
-              
-
-
               <Route path="/" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
