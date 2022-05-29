0e0bc932aaeedef5c35a7e16926b835eded88797
Hemant Gupta <gupta.hemant65@gmail.com>
2022-05-29 14:42:41 +0530

    update : change Face Register URL

--- src/components/FaceRegister.js
+++ src/components/FaceRegister.js
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import Webcam from "react-webcam";
 import axios from "axios";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Alert, Spinner } from "react-bootstrap";
 import { storage } from "../firebase";
-import { ref, getDownloadURL,uploadString } from 'firebase/storage';
+import { ref, getDownloadURL, uploadString } from "firebase/storage";
 import { useUserAuth } from "../context/UserAuthContext";
 import UserDataService from "../services/user.services";
 import { data } from "jquery";
-
-
 
 function RegisterFaceAuth(props) {
   //states for webcam
@@ -27,7 +25,7 @@ function RegisterFaceAuth(props) {
   const [StateOfProcess, setStateOfProcess] = useState("");
 
   //Spinner states
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
   //Message states
   const [show, setShow] = useState(false);
@@ -40,12 +38,11 @@ function RegisterFaceAuth(props) {
   const mobileNumber = props.mobileNumber;
   const { signUp } = useUserAuth();
 
- const id = "MSB" + Math.floor(100000 + Math.random() * 900000);
- const account_no = parseInt(Math.floor(10e10 + Math.random() * 900000000));
+  const id = "MSB" + Math.floor(100000 + Math.random() * 900000);
+  const account_no = parseInt(Math.floor(10e10 + Math.random() * 900000000));
 
-const dateCreated = new Date().toISOString();
-const dateUpdated = new Date().toISOString();
-
+  const dateCreated = new Date().toISOString();
+  const dateUpdated = new Date().toISOString();
 
   //method for capture an image Destop
   const captureImage = React.useCallback(async () => {
@@ -59,103 +56,92 @@ const dateUpdated = new Date().toISOString();
   }, [webcamRef, setImgSrc]);
 
   async function uploadImage(imgSrc) {
-    
     if (imgSrc !== null) {
       setStateOfProcess("Uploading...");
       const imgId = props.email.substring(0, props.email.lastIndexOf("@"));
       const fileName = imgId;
-      
-      const base64String = imgSrc.split(',')[1];
+
+      const base64String = imgSrc.split(",")[1];
       const imageRef = ref(storage, `facedata/${fileName}`);
 
-      uploadString(imageRef,base64String, "base64", {contentType: 'image/jpeg'})
-        .then((snapshot) => {
-       
+      uploadString(imageRef, base64String, "base64", {
+        contentType: "image/jpeg",
+      }).then((snapshot) => {
         setStateOfProcess("Processing...");
 
         getDownloadURL(snapshot.ref).then((urlFirebase) => {
-            setImageURL(urlFirebase);
+          setImageURL(urlFirebase);
 
-            const config = {
-                headers: {
-                  "Content-Type": "application/json",
-                  "Ocp-Apim-Subscription-Key":
-                  process.env.REACT_APP_AZURE_API_KEY,
-                },
-              }; 
-        
-              const newImageDetails = {
-                url: urlFirebase,
-              }; 
-        
-              axios
-                .post(
-                  "https://engagefaceapi.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_03&returnRecognitionModel=false&detectionModel=detection_02&faceIdTimeToLive=86400",
-                  newImageDetails,
-                  config
-                )
-                .then(async (response) => {
-                 
-                  setuserId(response.data[0].faceId);
-                  
-                  if(response.data.length===1)
-                  {const newUser = {
-                    dateCreated,
-                    dateUpdated,
-                    firstName,
-                    lastName,
-                    email,
-                    mobileNumber,
-                    id,
-                    account_no,
-                    balance
-                }
-                
-                      await signUp(email, password);
-                      await UserDataService.addUsers(newUser);
-                      
-        
-                  navigate("/home");
-                }else{
-                    if(response.data.length>1){
-                      props.enableModalCloseButton();
-                      setStateOfProcess("Multiple Face Found. Please try again.");
-                      setRetake(true);
-                      setLoading(false);
-                      setShow(true);
-                    }
-                  }
-                  
-                        
-                        
-                   })
-                .catch((err) => {
+          const config = {
+            headers: {
+              "Content-Type": "application/json",
+              "Ocp-Apim-Subscription-Key": process.env.REACT_APP_AZURE_API_KEY,
+            },
+          };
+
+          const newImageDetails = {
+            url: urlFirebase,
+          };
+
+          axios
+            .post(
+              "https://engagefaceapi.cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_03&returnRecognitionModel=false&detectionModel=detection_02&faceIdTimeToLive=86400",
+              newImageDetails,
+              config
+            )
+            .then(async (response) => {
+              setuserId(response.data[0].faceId);
+
+              if (response.data.length === 1) {
+                const newUser = {
+                  dateCreated,
+                  dateUpdated,
+                  firstName,
+                  lastName,
+                  email,
+                  mobileNumber,
+                  id,
+                  account_no,
+                  balance,
+                };
+
+                await signUp(email, password);
+                await UserDataService.addUsers(newUser);
+
+                navigate("/");
+              } else {
+                if (response.data.length > 1) {
                   props.enableModalCloseButton();
-                  setStateOfProcess("Face not found. Try again.");
+                  setStateOfProcess("Multiple Face Found. Please try again.");
                   setRetake(true);
                   setLoading(false);
                   setShow(true);
-                  // alert(err.message);
-                });
+                }
+              }
+            })
+            .catch((err) => {
+              props.enableModalCloseButton();
+              setStateOfProcess("Face not found. Try again.");
+              setRetake(true);
+              setLoading(false);
+              setShow(true);
+              // alert(err.message);
             });
-            
-        }
-      );
-    
-        
- } else {
-        props.enableModalCloseButton();
-        alert("First You Must Select An Image");
-        }
-
+        });
+      });
+    } else {
+      props.enableModalCloseButton();
+      alert("First You Must Select An Image");
     }
-    
+  }
 
   return (
     <div>
-      
-      {show ? <Alert className="text-center bg-white text-danger border-0">Face Not Detected</Alert> : null}
-     
+      {show ? (
+        <Alert className="text-center bg-white text-danger border-0">
+          Face Not Detected
+        </Alert>
+      ) : null}
 
       <div className="row text-center">
         <div className="col-md-12">
@@ -183,9 +169,19 @@ const dateUpdated = new Date().toISOString();
               Retake
             </button>
           ) : (
-            <div> <br/>
-              {loading ? <Spinner animation="border" className="mt-2" /> : <button className="btn btn-dark text-light" onClick={captureImage}>
-              Capture & Upload Image </button>}   
+            <div>
+              {" "}
+              <br />
+              {loading ? (
+                <Spinner animation="border" className="mt-2" />
+              ) : (
+                <button
+                  className="btn btn-dark text-light"
+                  onClick={captureImage}
+                >
+                  Capture & Upload Image{" "}
+                </button>
+              )}
             </div>
           )}
 
@@ -202,7 +198,5 @@ const dateUpdated = new Date().toISOString();
     </div>
   );
 }
-
-
 
 export default RegisterFaceAuth;
